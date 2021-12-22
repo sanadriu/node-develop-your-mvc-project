710f55eaf7001586ecd506db04975ca51800a6a0
sanadriu <skualed@gmail.com>
2021-12-22 12:42:52 +0100

    Save connection.js

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,2 +1,13 @@
 const mongoose = require("mongoose");
 const config = require("../config/config");
+
+function connection() {
+  return mongoose.connect(config.db.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  });
+}
+
+module.exports = connection;
