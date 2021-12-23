cd2d7ca13aae02010a5fecde642e2199d3329b03
andarbech <andarbech1.com>
2021-12-23 10:12:27 +0100

    pull originmain

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,9 +1,6 @@
-<<<<<<< HEAD
 const UserController = require("./UserController");
 
 module.exports = {
   UserController,
 };
-=======
 console.log("andrecito")
->>>>>>> e967974f60f159fd6d744c79d131d30472ed54e7
