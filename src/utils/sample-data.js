659ab8bed924129ca60c3874f739c128bd27e411
Daniella Alamo <dalamov08@gmail.com>
2022-01-12 17:33:50 +0100

    mobiles

--- src/utils/sample-data.js
+++ src/utils/sample-data.js
@@ -91,30 +91,85 @@ const users = [
 
 const products = [
   {
-    title: "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
-    price: 109.95,
-    stock: 3,
+    title: "Iphone 13",
+    price: 965,
+    stock: 30,
     description:
-      "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
-    images: ["https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"],
-  },
-  {
-    title: "Mens Casual Premium Slim Fit T-Shirts ",
-    price: 22.3,
-    stock: 5,
-    description:
-      "Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.",
+      "5.4 ”screen, A15 Bionic processor, 128GB, 256GB, 512GB storage, 6GB RAM, 12MP front camera, f / 2.2. Records video, at 4K and 60fps and 1080p and 120fps. 12MP rear camera, f / 1.6 (main), 12MP, f / 2.4 (wide angle). Records video at 4K and 60fps and 1080p and 240fps.",
     images: [
-      "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg",
+      "https://www.tuimeilibre.com/24597-large_default/apple-iphone-13-128gb-negro-libre.jpg",
+    ],
+  },
+
+  {
+    title: "Iphone 11 ",
+    price: 789,
+    stock: 21,
+    description:
+      "Apple launches its new smartphone, the iPhone 11 will make you feel like a real photographer and never slow down thanks to its camera and its powerful processor. The iPhone 11 carries Dual Cam technology, with two high-quality rear cameras, in addition to other features, such as a much more improved Siri service.",
+    images: [
+      "https://img.pccomponentes.com/articles/34/348193/1358-apple-iphone-11-128-gb-blanco-libre.jpg",
     ],
   },
   {
-    title: "Mens Cotton Jacket",
-    price: 55.99,
-    stock: 7,
+    title: "Samsung Galaxy S21",
+    price: 654,
+    stock: 17,
     description:
-      "great outerwear jackets for Spring/Autumn/Winter, suitable for many occasions, such as working, hiking, camping, mountain/rock climbing, cycling, traveling or other outdoors. Good gift choice for you or your family member. A warm hearted love to Father, husband or son in this thanksgiving or Christmas Day.",
-    images: ["https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg"],
+      "Galaxy S21 FE is designed for you, who are a fan of multimedia content, passionate about photography and / or games. Experience vivid viewing with its 6.4 ”2x Dynamic AMOLED screen, explore your world with its four cameras, or level up your game with its processor.",
+    images: [
+      "https://img.pccomponentes.com/articles/83/837005/1975-samsung-galaxy-s21-fe-5g-6-128gb-gris-libre.jpg",
+    ],
+  },
+  {
+    title: "Realme C21Y",
+    price: 210,
+    stock: 30,
+    description:
+      "5000 mAh Big Battery: More power. More life. This long-lasting battery lets you meet life's challenges. Power your day for longer with this massive 5000 mAh battery.",
+    images: [
+      "https://img.pccomponentes.com/articles/67/678167/1609-realme-c21y-3-32gb-negro-libre.jpg",
+    ],
+  },
+  {
+    title: "Xiaomi Mi 11i",
+    price: 613,
+    stock: 42,
+    description:
+      "My 11i, power that dazzles. This Xiaomi 5G mobile has a Qualcomm Snapdragon 888 flagship processor, a spectacular 120 Hz and 6.67 DotDisplay AMOLED screen and a 108 MP ultra-high resolution camera.",
+    images: [
+      "https://img.pccomponentes.com/articles/39/394962/193-xiaomi-mi-11i-5g-8-128gb-negro-libre.jpg",
+    ],
+  },
+  {
+    title: " iPhone XR",
+    price: 450,
+    stock: 35,
+    description:
+      "New Liquid Retina display with the most advanced LCD technology in the industry. Face ID even faster. The smartest and most powerful chip in a smartphone. And a revolutionary camera system. No matter where you look at it, the iPhone XR is simply amazing.",
+    images: [
+      "https://img.pccomponentes.com/articles/17/171054/iphone-xs-2-1747x3000-copia2.jpg",
+    ],
+  },
+  {
+    title: "Samsung Galaxy M52",
+    price: 320,
+    stock: 40,
+    description:
+      "Discover its wide 6.7 ”screen, with FHD + resolution and the latest Infinity- O sAMOLED + technology. This device has 3 rear cameras of the highest resolution and a 32 MP front camera to take sharper selfies. Enjoy for days thanks to its long-lasting battery!",
+    images: [
+      "https://img.pccomponentes.com/articles/61/616192/1633-samsung-galaxy-m52-5g-6-128gb-negro-libre.jpg",
+    ],
+  },
+  {
+    title: "Samsung Galaxy S21 FE",
+    price: 360,
+    stock: 40,
+    description:
+      "Do something epic every day with the Samsung Galaxy S21 FE 5G. With the 6.4 FHD + Dynamic AMOLED 2X smart screen. 3 rear cameras with professional results and a front camera for perfect selfies. A processor as fast as the way you consume your series. Sleek and slim body with IP68 certificate.",
+    images: [
+      "https://img.pccomponentes.com/articles/83/837017/1965-samsung-galaxy-s21-fe-5g-8-256gb-verde-libre.jpg",
+    ],
   },
 ];
 
